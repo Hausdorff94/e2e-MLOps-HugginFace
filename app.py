1924cf69b19f9105fe226fbf3bdc1718044be7ba
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-05-13 20:30:33 +0000

    Update app code

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ import gradio as gr
 
 model = pipeline("summarization")
 
+
 def predict(text):
-    return model(text)[0]['summary_text']
+    return model(text)[0]["summary_text"]
 
 
-with gr.Blocks() as block:
-    textbox = gr.Textbox(placeholder="Paste your text here...", lines=5, label="Input Text")
-    gr.Interface(fn=predict, inputs=textbox, outputs="text", title="Summarizer", description="Summarize your text with Hugging Face Transformers", article="https://huggingface.co/transformers/model_doc/bart.html")
+with gr.Blocks() as myblock:
+    textbox = gr.Textbox(
+        placeholder="Paste your text here...", lines=5, label="Input Text"
+    )
+    gr.Interface(
+        fn=predict,
+        inputs=textbox,
+        outputs="text",
+        title="Summarizer",
+        description="Summarize your text with Hugging Face Transformers",
+        article="https://huggingface.co/transformers/model_doc/bart.html",
+    )
 
-block.launch()
+myblock.launch()
